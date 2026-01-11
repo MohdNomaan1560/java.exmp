550cc299c5cf467ef0bef87106239a6160d57b46
MohdNomaan1560 <133195229+MohdNomaan1560@users.noreply.github.com>
2026-01-11 10:22:26 +0530

    Fix syntax error in main method declaration

--- Test.java
+++ Test.java
@@ -8,7 +8,7 @@ public class Test {
         int res=a+b+c;
         System.out.println("the result is " +res);
     }
-    public static void main(String[] args)
+    public static void main(String[] args).
     {
         add();
     }
